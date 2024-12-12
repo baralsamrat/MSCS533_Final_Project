9e65f7971443673595e633414e079c21b3ac54f2
baralsamrat <sbaral30064@ucumberlands.edu>
2024-12-12 16:44:49 -0600

    Initial Post

---
+++ src/LocationTrackingApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LocationTrackingApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
